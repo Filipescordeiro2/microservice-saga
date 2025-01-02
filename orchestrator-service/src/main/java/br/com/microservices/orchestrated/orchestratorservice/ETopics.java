944f1ab8346d15ca37c4db7603a7858b1cadda6a
Filipescordeiro2 <filipescordeiro@hotmail.com>
2025-01-02 14:13:53 -0300

    implementacao do kafka config

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/ETopics.java
@@ -0,0 +1,24 @@
+package br.com.microservices.orchestrated.orchestratorservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public enum ETopics {
+    START_SAGA("start-saga"),
+    BASE_ORCHESTRATOR("base-orchestrator"),
+    FINISH_SUCCESSE_TOPICS("finish-successe"),
+    FINISH_FAIL("finish-fail"),
+    PRODUCT_VALIDATION_SUCCESS("finish-product-validation-success"),
+    PRODUCT_VALIDATION_FAIL("finish-product-validation-fail"),
+    PAYMENT_SUCCESS("finish-payment-success"),
+    PAYMENT_FAIL("finish-payment-fail"),
+    INVENTORY_SUCCESS("finish-inventory-success"),
+    INVENTORY_FAIL("finish-inventory-fail"),
+    NOTIFY_ENDING("notify-ending");
+
+    private String topic;
+}
