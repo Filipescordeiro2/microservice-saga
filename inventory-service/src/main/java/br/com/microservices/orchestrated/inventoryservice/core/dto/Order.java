6b04320cf0de8c8cb5e881541bf0db11d9ba80fa
Filipescordeiro2 <filipescordeiro@hotmail.com>
2025-01-02 22:20:27 -0300

    criacao das camadas de cada servico

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/dto/Order.java
@@ -0,0 +1,25 @@
+package br.com.microservices.orchestrated.inventoryservice.core.dto;
+
+import br.com.microservices.orchestrated.orchestratorservice.core.dto.OrderProducts;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Order {
+
+    private String id;
+    private List<OrderProducts> products;
+    private LocalDateTime createAt;
+    private String TransactionId;
+    private double totalAmount;
+    private int totalItems;
+
+}
