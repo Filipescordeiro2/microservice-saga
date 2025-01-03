7c883bf9e7a51b922b67f1f50c3f050dd099fd5e
Filipescordeiro2 <filipescordeiro@hotmail.com>
2025-01-03 11:04:54 -0300

    Implementado a consummer e Producer e tambem as execptions

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/producer/SagaOrchestratorProducer.java
@@ -0,0 +1,24 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.producer;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@AllArgsConstructor
+@Component
+public class SagaOrchestratorProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendEvent(String paylod,String topic) {
+
+        try {
+            log.info("Sending event to topic: {} with data {}", topic,paylod);
+            kafkaTemplate.send(topic, paylod);
+        } catch (Exception e) {
+            log.error("Error trying to send event to topic: {} with data {}", topic,paylod, e);
+        }
+    }
+}
